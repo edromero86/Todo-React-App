6a50fe585033e92e9eea46b5db0b3d77e8e800e3
Edgar Romero <edgrom86@gmail.com>
2022-12-21 08:31:09 -0600

    add todo function, react hooks and icons

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,17 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function TodoForm(props) {
   const [input, setInput] = useState('');
 
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
+
   const handleChange = e => {
     setInput(e.target.value);
-  }
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -27,6 +33,7 @@ function TodoForm(props) {
         name='text' 
         className='todo-input'
         onChange={handleChange}
+        ref={inputRef}
       />
       <button className='todo-button'>Add todo</button>
     </form>
